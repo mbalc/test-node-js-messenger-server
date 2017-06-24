a1a42b165253048950ee2f47ad3a2fbc8f370496
Michał Balcerzak <michal@radcode.co>
2017-06-24 09:01:29 +0200

    post/get requests handling

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+const serverPort = 8080;
+
 const express = require('express');
-const http = require('http');
 const pg = require('pg');
 
-
 const app = express();
-const userBase = pg.create();
 
-app.post('/recipients', (req, res) => {
-})
+pg.query('CREATE LOCAL TEMP TABLE userBase (id int)');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Hello Http');
+function AddUser(pay, res) {
+  console.log('adding user...');
+  // here we should decompose $pay in some way
+  pg.query('INSERT INTO userBase ');
+  res.touch();
+}
+
+function ListUsers(pay, res) {
+  console.log('listing users...');
+  res.touch();
+}
+
+function SendMessage(pay, res) {
+  console.log('sending user a message...');
+  res.touch();
+}
+
+app.post('/recipients', AddUser);
+app.get('/recipients', ListUsers);
+app.post('/send', SendMessage);
+
+app.listen(serverPort, () => {
+  console.log(`Messenger server listening on port${serverPort.toString()}!`);
 });
-server.listen(8080);
