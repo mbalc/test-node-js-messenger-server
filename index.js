f2cfaa49748d9901654618acda880f0f1f914f88
Michał Balcerzak <michal@radcode.co>
2017-06-25 15:55:57 +0200

    proper add/get recipients postgres support

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const serverPort = 8080;
+const config = require('./config.json');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const pg = require('pg');
 
 const app = express();
-const client = new pg.Client();
+const client = new pg.Client(`postgres://${config.pgUsername}:${config.pgPassword}
+  @${config.serverHostPath}:${config.pgPort}/${config.pgDatabaseName}`);
 
 app.use(bodyParser.text());
 
-client.query('CREATE LOCAL TEMP TABLE userBase (id int)');
+client.connect();
+client.query('CREATE TABLE IF NOT EXISTS userBase (id int)');
+client.query('TRUNCATE TABLE userBase');
 
 function AddUser(pay, res) {
   console.log('adding a user...');
@@ -17,6 +20,8 @@ function AddUser(pay, res) {
   try {
     reqBodyObj = JSON.parse(pay.body);
   } catch (e) {
+    console.log('Invalid request body in form of');
+    console.log(pay.body);
     return res
       .status(400)
       .send('Request body not a valid JSON object / parseable string!');
@@ -25,12 +30,19 @@ function AddUser(pay, res) {
   if ('id' in reqBodyObj) {
     const userID = reqBodyObj.id;
 
-    // todo make checking if ID already exists in userBase
-
-    client.query(`INSERT INTO userBase (id) VALUES (${userID})`);
-    return res
-      .status(200)
-      .send(`User of ID=${userID} successfully added to database!`);
+    return client.query(`SELECT 1 FROM userBase WHERE id = ${userID}`, (err, out) => {
+      console.log(out.rows);
+      if (out.rows.size() > 0) { // todo need proper empty array check
+        res
+          .status(400)
+          .send(`User of ID=${userID} has already been added to database, ignoring request...`);
+      } else {
+        client.query(`INSERT INTO userBase (id) VALUES (${userID})`);
+        res
+          .status(200)
+          .send(`User of ID=${userID} successfully added to database!`);
+      }
+    });
   }
 
   return res
@@ -40,7 +52,11 @@ function AddUser(pay, res) {
 
 function ListUsers(pay, res) {
   console.log('listing users...');
-  res.send(client.query('SELECT * FROM userBase'));
+  client.query('SELECT * FROM userBase', (err, out) => {
+    res
+      .status(200)
+      .send(out.rows);
+  });
 }
 
 function SendMessage(pay, res) {
@@ -51,6 +67,6 @@ app.post('/recipients', AddUser);
 app.get('/recipients', ListUsers);
 app.post('/send', SendMessage);
 
-app.listen(serverPort, () => {
-  console.log(`Messenger server listening on port ${serverPort.toString()}!`);
+app.listen(config.serverPort, () => {
+  console.log(`Messenger server listening on port ${config.serverPort.toString()}!`);
 });
