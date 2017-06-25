974d52d53cee215eb7ec1fd85f4fcba2a60d8519
Michał Balcerzak <michal@radcode.co>
2017-06-25 10:48:56 +0200

    add new recipient support (buggy)

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 const serverPort = 8080;
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const pg = require('pg');
 
 const app = express();
 const client = new pg.Client();
 
+app.use(bodyParser.text());
+
 client.query('CREATE LOCAL TEMP TABLE userBase (id int)');
 
 function AddUser(pay, res) {
-  res.send(pay.query.id);
-  // const user = JSON.parse(pay);
-  // console.log(`adding user ${user.id}...`);
-  // here we should decompose $pay in some way
-  // pg.query('INSERT INTO userBase ');
+  console.log('adding a user...');
+  let reqBodyObj = {};
+  try {
+    reqBodyObj = JSON.parse(pay.body);
+  } catch (e) {
+    return res
+      .status(400)
+      .send('Request body not a valid JSON object / parseable string!');
+  }
+
+  if ('id' in reqBodyObj) {
+    const userID = reqBodyObj.id;
+
+    // todo make checking if ID already exists in userBase
+
+    client.query(`INSERT INTO userBase (id) VALUES (${userID})`);
+    return res
+      .status(200)
+      .send(`User of ID=${userID} successfully added to database!`);
+  }
+
+  return res
+    .status(400)
+    .send('Request object doesn\'t contain "id" key!');
 }
 
 function ListUsers(pay, res) {
   console.log('listing users...');
-  res.json(client.query('SELECT * FROM userBase'));
+  res.send(client.query('SELECT * FROM userBase'));
 }
 
 function SendMessage(pay, res) {
