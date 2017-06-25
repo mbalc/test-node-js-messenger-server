d51eec1028149e8e30dfd72dc7b0409997b6350b
Michał Balcerzak <michal@radcode.co>
2017-06-25 20:54:34 +0200

    add first support for message sending, minor refactor

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const config = require('./config.json');
 const express = require('express');
 const bodyParser = require('body-parser');
 const pg = require('pg');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
 
 const app = express();
 const client = new pg.Client(`postgres://${config.pgUsername}:${config.pgPassword}
@@ -12,7 +14,7 @@ app.use(bodyParser.text());
 
 client.connect();
 client.query('DROP TABLE IF EXISTS userBase');
-client.query('CREATE TABLE IF NOT EXISTS userBase (id int)');
+client.query('CREATE TABLE IF NOT EXISTS userBase (id bigint)');
 
 // finds value assigned to $key in the body of a given payload
 // if the key was not found:
@@ -44,13 +46,11 @@ function PullProp(pay, res, key) {
 
 function AddUser(pay, res) {
   console.log('adding a user...');
-  console.log(PullProp(pay, res, 'id'));
-
   const userID = PullProp(pay, res, 'id');
   if (userID != null) {
-    return client.query(`SELECT 1 FROM userBase WHERE id = ${userID}`, (err, out) => {
+    client.query(`SELECT 1 FROM userBase WHERE id = ${userID}`, (err, out) => {
       console.log(out.rows);
-      if (out.rows.length > 0) { // todo need proper empty array check
+      if (out.rows.length > 0) {
         res
           .status(400)
           .send(`User of ID=${userID} has already been added to database, ignoring request...`);
@@ -75,6 +75,33 @@ function ListUsers(pay, res) {
 
 function SendMessage(pay, res) {
   console.log('sending users a message...');
+  const message = PullProp(pay, res, 'message');
+  client.query('SELECT * FROM userBase', (err, out) => {
+    out.rows.forEach((obj) => {
+      const data = {
+        recipient: {
+          id: `${obj.id}`,
+        },
+        message: {
+          text: message,
+        },
+      };
+      console.log(data);
+      const request = new XMLHttpRequest();
+      request.open('POST', `https://graph.facebook.com/v2.6/me/messages?access_token=${config.pageAccessToken}`, true);
+      request.setRequestHeader('Content-Type', 'application/json');
+      // subscribe to this event before you send your request.
+      request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+   // alert the user that a response now exists in the responseTest property.
+          console.log(request.responseText);
+   // And to view in firebug
+          console.log('xhr', request);
+        }
+      };
+      request.send(JSON.stringify(data));
+    });
+  });
 }
 
 app.post('/recipients', AddUser);
